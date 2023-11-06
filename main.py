90056bfd47accc0d1e3c81c7ecc2a16be7b88f21
Pascal378 <pascal.schwartz@protonmail.com>
2023-11-06 21:33:28 +0100

    Add simple client functionality

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import pyglet
+from pyglet.window import key
+import socketio
 
-game_window = pyglet.window.Window(800, 600)
+# PyGlet Window
+window = pyglet.window.Window(800, 600)
+
+# Label
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+# Erstelle SocketIO Client
+sio = socketio.Client()
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+# Event-Handler für Antworten vom Server
+@sio.event
+def response(data):
+    print(f"Server response: {data}")
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.A:
+        sio.emit('message', "A pressed")
+        label.text = "A pressed"
 
 if __name__ == '__main__':
+    sio.connect('http://localhost:8080')
     pyglet.app.run()
+
+
+
