f91c6d0b10c6ab82ffe25e4cd32a862754282d03
Simon Gartzke <simon.gartzke@icloud.com>
2023-11-09 20:15:11 +0100

    Restructed the Files and removed Server-Client funcions

--- main.py
+++ main.py
@@ -1,37 +1,7 @@
 import pyglet
-from pyglet.window import key
-import socketio
 
-# PyGlet Window
-window = pyglet.window.Window(800, 600)
+from menu.gamemenu import GameMenu
 
-# Label
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
-# Erstelle SocketIO Client
-sio = socketio.Client()
-
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
-# Event-Handler für Antworten vom Server
-@sio.event
-def response(data):
-    print(f"Server response: {data}")
-
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.A:
-        sio.emit('message', "A pressed")
-        label.text = "A pressed"
-
-if __name__ == '__main__':
-    sio.connect('http://localhost:8080')
+if __name__ == "__main__":
+    GameMenu()
     pyglet.app.run()
-
-
-
