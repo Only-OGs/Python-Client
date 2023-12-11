75f0ba73e1509df73c34289a5e2342ef1c87cdf2
phil <phil.c137.git@gmail.com>
2023-12-11 20:28:51 +0100

    fixed HUD

--- rendering/gui.py
+++ rendering/gui.py
@@ -53,15 +53,18 @@ class Gui:
         #Print die Bestzeit
         font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
         text = font.render("schnellste Runde {}:{}:{}".format(self.rec_min, self.rec_sec, self.rec_mil_sec), True
-                           ,(var.VIOLETTE))
-        self.background_gui(x=var.width // 2 - 175, y=0, width=300)
+                           ,(var.BLACK))
+        self.background_gui(x=var.width // 2 - 175, y=0, width=300, color=var.TRANSPARENT_WHITE)
         self.screen.blit(text, (var.width // 2 - 150, 0 + self.text_offste))
 
         # Print die den Runden counter
         font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
-        text = font.render("Time: {}:{}:{}".format(self.min, self.sec, self.mil_sec), True, (var.VIOLETTE))
-        self.background_gui(x=0, y=0, width=150)
+        text = font.render("Time: {}:{}:{}".format(self.min, self.sec, self.mil_sec), True, (var.BLACK))
+        self.background_gui(x=0, y=0, width=150, color=var.TRANSPARENT_WHITE)
         self.screen.blit(text, (0, 0 + self.text_offste))
+
+        #Red Background
+        self.background_gui(x=0, y=0, width=var.width, color= var.TRANSPARENT_RED)
 
     #Beendet den Timer und setzt die neue bestzeit
     def ende_timer(self):
@@ -90,14 +93,16 @@ class Gui:
     #Zeigt dem Spieler seine Gewschwindigkeit
     def show_speed(self, speed):
         font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
-        text = font.render("{} kmh".format(round(speed/100)), True, (var.VIOLETTE))
-        self.background_gui(x=var.width-150, y=0, width=150)
+        text = font.render("{} kmh".format(round(speed/100)), True, (var.BLACK))
+        self.background_gui(x=var.width-150, y=0, width=150,color=var.TRANSPARENT_WHITE)
         self.screen.blit(text, (var.width-150, self.text_offste))
 
     #Übermalt die Alten GUI elemente damit diese sich nicht Stacken.
-    def background_gui(self, x, y, width):
+    def background_gui(self, x, y, width, color):
         rect = pygame.rect.Rect(x, y, width, 40)
-        pygame.draw.rect(self.screen, var.TRANSPARENT_BLACK, rect)
+        shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+        pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+        self.screen.blit(shape_surf, rect)
 
     def reset_lap(self):
         self.min = 0
