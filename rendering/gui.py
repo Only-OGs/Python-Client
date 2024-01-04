a1e35999189e122b3e35db768c1298a9f7fe8f3e
phil <phil.c137.git@gmail.com>
2024-01-04 16:55:10 +0100

    fixed timer and added lap

--- rendering/gui.py
+++ rendering/gui.py
@@ -31,36 +31,47 @@ class Gui:
 
     #Zählt den Timer hoch, muss im Game-Loop aufgerufen werden
     def count_up(self):
-        self.mil_sec += self.add
-        if self.mil_sec == 60:
-            self.mil_sec = 0
-            self.sec += 1
-
-        if self.sec == 60:
-            self.mil_sec = 0
-            self.sec = 0
-            self.min += 1
-
-        if self.min == 60:
-            self.mil_sec = 0
-            self.sec = 0
-            self.min = 0
-
-        #Print die Bestzeit
         font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
-        text = font.render("schnellste Runde {}:{}:{}".format(self.rec_min, self.rec_sec, self.rec_mil_sec), True
-                           ,(var.BLACK))
-        self.background_gui(x=var.width // 2 - 175, y=0, width=300, color=var.TRANSPARENT_WHITE)
-        self.screen.blit(text, (var.width // 2 - 150, 0 + self.text_offste))
+        # Multiplayer layout
+        if var.client.sio.connected:
+            text = font.render("schnellste Runde {}".format(var.best_time), True
+                               ,(var.BLACK))
+            self.background_gui(x=var.width // 2 - 175, y=0, width=300, color=var.TRANSPARENT_WHITE, length=40)
+            self.screen.blit(text, (var.width // 2 - 150, 0 + self.text_offste))
 
-        # Print die den Runden counter
-        font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
-        text = font.render("Time: {}:{}:{}".format(self.min, self.sec, self.mil_sec), True,(var.BLACK))
-        self.background_gui(x=0, y=0, width=150, color=var.TRANSPARENT_WHITE)
-        self.screen.blit(text, (0, 0 + self.text_offste))
+            # schreibt die aktuelle Zeit
+            text = font.render("Time: {}".format(var.current_time), True,(var.BLACK))
+            self.background_gui(x=0, y=0, width=200, color=var.TRANSPARENT_WHITE, length=40)
+            self.screen.blit(text, (0, 0 + self.text_offste))
 
-        #Red Background
-        self.background_gui(x=0, y=0, width=var.width, color=var.TRANSPARENT_RED)
+
+        # Singleplayer layout
+        else:
+
+            self.mil_sec += self.add
+            if self.mil_sec == 60:
+                self.mil_sec = 0
+                self.sec += 1
+
+            if self.sec == 60:
+                self.mil_sec = 0
+                self.sec = 0
+                self.min += 1
+
+            if self.min == 60:
+                self.mil_sec = 0
+                self.sec = 0
+                self.min = 0
+
+            text = font.render("schnellste Runde {}:{}:{}".format(self.rec_min, self.rec_sec, self.rec_mil_sec), True
+                               , (var.BLACK))
+            self.background_gui(x=var.width // 2 - 175, y=0, width=300, color=var.TRANSPARENT_WHITE, length=40)
+            self.screen.blit(text, (var.width // 2 - 150, 0 + self.text_offste))
+
+            # Print die den Runden counter
+            text = font.render("Time: {}:{}:{}".format(self.min, self.sec, self.mil_sec), True, (var.BLACK))
+            self.background_gui(x=0, y=0, width=200, color=var.TRANSPARENT_WHITE, length=40)
+            self.screen.blit(text, (0, 0 + self.text_offste))
 
     #Beendet den Timer und setzt die neue bestzeit
     def ende_timer(self):
@@ -87,12 +98,16 @@ class Gui:
     def show_speed(self, speed):
         font = pygame.font.SysFont("assets/rocket-rinder-font/RocketRinder-yV5d.ttf", 32)
         text = font.render("{} kmh".format(round(speed/100)), True, (var.BLACK))
-        self.background_gui(x=var.width-150, y=0, width=150,color=var.TRANSPARENT_WHITE)
+        if var.client.sio.connected:
+            textRunde = font.render("Runde: {}".format(var.lap), True, (var.BLACK))
+        else:
+            textRunde = font.render("Runde: {}".format(var.lap_count), True, (var.BLACK))
+        self.background_gui(x=var.width-150, y=0, width=150,color=var.TRANSPARENT_WHITE, length=80)
         self.screen.blit(text, (var.width-150, self.text_offste))
-
+        self.screen.blit(textRunde, (var.width - 150, self.text_offste + 40))
     #Übermalt die Alten GUI elemente damit diese sich nicht Stacken.
-    def background_gui(self, x, y, width, color):
-        rect = pygame.rect.Rect(x, y, width, 40)
+    def background_gui(self, x, y, width, color, length):
+        rect = pygame.rect.Rect(x, y, width, length)
         shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
         pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
         self.screen.blit(shape_surf, rect)
